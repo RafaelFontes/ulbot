c88294e314eedd34b8bf9e9547cd48848db1808c
Rafael <rafael.fontes@integritas.net>
2018-11-11 04:19:50 -0200

    removing config.json from git

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,3 @@ import { DiscordClient } from './discord-client';
 const discordClient = installer.resolve<DiscordClient>(DiscordClient);
 
 discordClient.connect();
-
-
-/*import * as Discord from 'discord.js';
-import { Config } from './config';
-const client = new Discord.Client();
-=
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
-});
-
-client.login('NTExMDM4Mzc5NzYxMzM2MzIx.DslFqg.mIpwn5wLkAft5ewUCS56INWRz4E');
-*/
